fef2b1ad0fc319da30ede4efa3976e910dac5e5d
joaobfuzinelli <160135328+joaobfuzinelli@users.noreply.github.com>
2024-02-19 17:26:14 -0300

    Update webscrapping.py

--- webscrapping.py
+++ webscrapping.py
@@ -11,7 +11,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
 }
 
-# Lista de URLs (Para verificar)
+# HREF (Para verificar)
 href = "Dados_abertos_Nao_Previdenciario.zip"
 url_list = [url]
 
